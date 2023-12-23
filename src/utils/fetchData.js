a91e04d75549973e5ba8a439b3f1fee324d49b3d
Ishank Chopra <ishankchopranbd@gmail.com>
2023-12-23 23:36:35 +0530

    Make Detail Page

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -7,6 +7,15 @@ export const exerciseOptions = {
   },
 };
 
+export const youtubeOptions = {
+  method: "GET",
+
+  headers: {
+    "X-RapidAPI-Key": "bd43abc361msh92aff9ddef954e0p124d25jsn051e37383506",
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+  },
+};
+
 export const fetchData = async (url, options) => {
   const response = await fetch(url, options);
   const data = await response.json();
